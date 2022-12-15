fee794d4dd9413d9ff423e3d006c88685fca055b
rahul620240 <rahul620240@gmail.com>
2022-12-15 11:38:46 +0530

    one

---
+++ app.js
@@ -0,0 +1,13 @@
+//NORMAL FUNCTION
+var person=function(a,b){
+var c =a+b;
+  console.log("addition of two no is "+c)
+}
+
+person(3,6);
+
+
+
+//arrow function 
+
+var users=()=>console.log("this is a arrow function");
